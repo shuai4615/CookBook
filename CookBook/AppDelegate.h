202006f28114771f09bfb81a12a9315812d139f2
qianfeng <qianfeng@FS.local>
2015-07-03 12:51:38 +0800

    Initial Commit

---
+++ CookBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookBook
+//
+//  Created by qianfeng on 15/7/3.
+//  Copyright (c) 2015年 FanShuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
